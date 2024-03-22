9a1d3d6e41a5138fa98660ab029d810ce0cdfd84
Rajput-vinay <vinayrajput2004vr@gmail.com>
2024-03-22 10:46:52 +0530

    conversion

---
+++ 01-basics/03_Conversion_operation.js
@@ -0,0 +1,36 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "vinay"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  //print NAN
+
+
+
+// "33" => 33
+// "33abc" => NAN
+// true => 1
+
+
+// let isLoaggedIn = 1;  // => true
+// let isLoaggedIn = ""   // => false
+let isLoaggedIn = "vinay" // => true
+let booleanIsLoggedIn = Boolean(isLoaggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
