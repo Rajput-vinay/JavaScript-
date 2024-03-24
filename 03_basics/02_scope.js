42052ba3a9504cf2037809eff2339ab0610d0028
Rajput-vinay <vinayrajput2004vr@gmail.com>
2024-03-24 12:36:01 +0530

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,13 @@
+
+// var c =300   // never used again
+let a = 300
+
+if(true){
+    let a = 10
+    const b =20
+    console.log("INNER: " , a)
+    // var c =30 
+}
+console.log(a)
+// console.log(b)
+// console.log(c)
